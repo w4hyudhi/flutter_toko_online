8b9905e2cb7aab26e7ab353fc07030bf5012e748
wahyudhi <mahfudwahyudi.k@gmail.com>
2023-07-07 19:05:13 +0900

    home header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_toko_online/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('home'),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Halo Alex',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  Text(
+                    '@alex',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/image_profile.png'))),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
