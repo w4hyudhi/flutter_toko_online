7b7a18bdac09f10e1c45b6171023dd110f0862a5
wahyudhi <mahfudwahyudi.k@gmail.com>
2023-07-11 14:51:08 +0900

    header chat detail

--- lib/pages/widgets/chat_tile.dart
+++ lib/pages/widgets/chat_tile.dart
@@ -6,50 +6,55 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: defaultMargin),
-      child: Column(children: [
-        Row(
-          children: [
-            Image.asset(
-              'assets/image_shop_logo.png',
-              width: 54,
-            ),
-            const SizedBox(
-              width: 12,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Souse Store',
-                    style: primaryTextStyle.copyWith(fontSize: 15),
-                  ),
-                  Text(
-                    'good night HOw do you do tomorrow',
-                    style: secondaryTextStyle.copyWith(
-                      fontWeight: light,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  )
-                ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail-chat');
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: Column(children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo.png',
+                width: 54,
               ),
-            ),
-            Text(
-              'Now',
-              style: secondaryTextStyle.copyWith(fontSize: 10),
-            )
-          ],
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        const Divider(
-          thickness: 1,
-          color: Color(0xff2B2939),
-        )
-      ]),
+              const SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Souse Store',
+                      style: primaryTextStyle.copyWith(fontSize: 15),
+                    ),
+                    Text(
+                      'good night HOw do you do tomorrow',
+                      style: secondaryTextStyle.copyWith(
+                        fontWeight: light,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    )
+                  ],
+                ),
+              ),
+              Text(
+                'Now',
+                style: secondaryTextStyle.copyWith(fontSize: 10),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color(0xff2B2939),
+          )
+        ]),
+      ),
     );
   }
 }
