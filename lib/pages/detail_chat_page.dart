40b2db0d8bf7933f39a31dca281f67e389c8cea3
wahyudhi <mahfudwahyudi.k@gmail.com>
2023-07-30 20:32:49 +0900

    widget product preview

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -46,39 +46,100 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
-    Widget chatInput() {
+    Widget productPreview() {
       return Container(
-        margin: const EdgeInsets.all(20),
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(bottom: 20),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: backgroundColor5,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: primaryColor)),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                decoration: BoxDecoration(
-                  color: backgroundColor4,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: TextFormField(
-                    style: primaryTextStyle,
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Type Message...',
-                      hintStyle: subtitleTextStyle,
-                    ),
-                  ),
-                ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
               ),
             ),
             SizedBox(
-              width: 20,
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'COURT VISION SHOES',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,00',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
+              ),
             ),
             Image.asset(
-              'assets/button_send.png',
-              width: 45,
+              'assets/button_close.png',
+              width: 22,
             )
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                )
+              ],
+            ),
           ],
         ),
       );
