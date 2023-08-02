ea00be32c7d7a346b59e18d334e8c5bb04df1879
wahyudhi <mahfudwahyudi.k@gmail.com>
2023-08-02 10:48:25 +0900

    header and content profile

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,12 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_toko_online/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile'),
+    Widget header() {
+      return AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(defaultMargin),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/image_profile.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'hallo allext',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 24, fontWeight: semiBold),
+                      ),
+                      Text(
+                        '@alexkeing',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/button_exit.png',
+                  width: 20,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget menuItem(String text) {
+      return Container(
+        margin: EdgeInsets.only(top: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: secondaryTextStyle.copyWith(fontSize: 13),
+            ),
+            const Icon(
+              Icons.chevron_right,
+              color: Color(0xffF1F0F2),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+          child: Container(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        width: double.infinity,
+        color: backgroundColor3,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Account',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+            ),
+            menuItem('Edit Profile'),
+            menuItem('Your Orders'),
+            menuItem('Help'),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'General',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+            ),
+            menuItem('Privacy & Policy'),
+            menuItem('Term of Service'),
+            menuItem('Rate App'),
+          ],
+        ),
+      ));
+    }
+
+    return Column(
+      children: [header(), content()],
     );
   }
 }
